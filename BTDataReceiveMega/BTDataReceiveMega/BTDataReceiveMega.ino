90f397db78c0b1b2122a2a05e920a740889d994c
SoyUnCitrico <1darenastolteca@gmail.com>
2022-04-27 12:31:00 -0500

    Cambios en el codigo de control final a MEGA

--- BTDataReceiveMega/BTDataReceiveMega/BTDataReceiveMega.ino
+++ BTDataReceiveMega/BTDataReceiveMega/BTDataReceiveMega.ino
@@ -1,22 +1,39 @@
+String lecturaBT;
+int ledPIN = 13;
+
 void setup()
 {   Serial1.begin(115200);
-    Serial.println("INICIANDO BT...");
+    Serial1.write("INICIANDO BT...");
     Serial.begin(115200);
     Serial.println("INICIANDO USB...");
+    pinMode(ledPIN, OUTPUT);
     delay(100);
 }
 
-void loop()
-{ //Envíamos y recibimos datos
-   if (Serial.available() > 0) {
-       Serial1.write(Serial.read());
-       Serial1.write("DATA");
-       
-   }
+void loop() { 
+    if(Serial.available() > 0) {
+      Serial1.write(Serial.read());
+      Serial.flush();
+    }
    if (Serial1.available() > 0) {
-       Serial.write(Serial1.read());
-       int n = Serial1.readString().toInt();
-       if(n === 0) {Serial.println(n);}
-       else { Serial.println("No eres lo que busco");}
+    //Serial1.println("Reescribiendo");
+    lecturaBT = Serial1.readString();
+    
+    Serial1.println(lecturaBT);
+    Serial1.println(lecturaBT.length());
+    
+    if (lecturaBT.length() == 3) {
+      int encendido = lecturaBT.toInt();
+      if(encendido == 0) {
+        Serial.println("Apagar TELEFONO");
+        digitalWrite(ledPIN, LOW);
+      } else if (encendido == 1) {
+        Serial.println("Encender TELEFONO");
+        digitalWrite(ledPIN, HIGH);
+      }
+    } else {
+    Serial.println(lecturaBT);
+    Serial1.flush();
    }
+  }
 }
